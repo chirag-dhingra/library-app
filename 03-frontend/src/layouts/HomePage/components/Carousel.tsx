7a4a55182008f773e9c7639d811e6137ee2d897a
dhingra <chirag.dhingra@adesso.de>
2023-12-06 16:51:19 +0100

    fetch books and useEffet function implemented

--- 03-frontend/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/src/layouts/HomePage/components/Carousel.tsx
@@ -8,7 +8,33 @@ export const Carousel = () => {
 
     useEffect(() => {
      const fetchBooks = async() => {
+        const baseUrl: string = "http://localhost:8080/api/books"
+        const url: string = `${baseUrl}?page=0&size=9`
+        const response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error('Something went wrong!')
+        }
         
+        const responseJson = await response.json();
+
+        const responseData = responseJson._embedded.books;//All the books are here
+
+        const loadedBooks: BookModel[] = [];
+
+        for(const key in responseData){
+            loadedBooks.push({
+                id: responseData[key].id,
+                title: responseData[key].title,
+                author: responseData[key].author,
+                description: responseData[key].description,
+                copies: responseData[key].copies,
+                copiesAvailable: responseData[key].copiesAvailable,
+                img: responseData[key].img
+            })
+        }
+        setBooks(loadedBooks);
+        setIsLoading(false);
      };
      fetchBooks().catch((error: any) => {
        setIsLoading(false);
