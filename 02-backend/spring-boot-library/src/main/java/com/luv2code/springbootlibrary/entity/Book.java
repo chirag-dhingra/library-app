1543d3a877f8887417d49ee00c0ca9899fc629c5
dhingra <chirag.dhingra@adesso.de>
2023-12-06 15:05:36 +0100

    Backend added

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/entity/Book.java
@@ -0,0 +1,44 @@
+package com.luv2code.springbootlibrary.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+@Entity
+@Table
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
